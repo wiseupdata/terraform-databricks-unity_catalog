4d4e963b9fa6a8257fd72dfcb2fed60f4513a45e
silllvio <silllvio@hotmail.com>
2023-03-26 22:10:29 +0100

    sync

--- data.tf
+++ data.tf
@@ -14,7 +14,7 @@ data "azuread_client_config" "current" {}
 
 
 data "azurerm_storage_account" "ext_storages" {
-  count = var.ext_storages
+  count               = length(var.ext_storages)
   name                = var.ext_storages[count.index].stg_name
   resource_group_name = var.ext_storages[count.index].rg_name
 }
