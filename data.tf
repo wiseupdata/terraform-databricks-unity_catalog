6c6d782e09bb6edd3a9f6c78b758a0a17dbc1af1
silllvio <silllvio@hotmail.com>
2023-03-26 22:08:42 +0100

    sync

--- data.tf
+++ data.tf
@@ -12,7 +12,9 @@ data "azurerm_databricks_workspace" "this" {
 
 data "azuread_client_config" "current" {}
 
-data "azurerm_storage_account" "this" {
-  name                = var.ext_storages[0].stg_name
-  resource_group_name = var.ext_storages[0].rg_name
+
+data "azurerm_storage_account" "ext_storages" {
+  count = var.ext_storages
+  name                = var.ext_storages[count.index].stg_name
+  resource_group_name = var.ext_storages[count.index].rg_name
 }
